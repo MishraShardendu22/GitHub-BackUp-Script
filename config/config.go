3490e9e33f69f5db23ff7127606097fb43768ced
MishraShardendu22 <mishrashardendu22@gmail.com>
2026-05-25 21:37:55 +0530

    refactor: made the codebase abstract for easier understanding
    Signed-off-by: MishraShardendu22 <mishrashardendu22@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"github.com/MishraShardendu22/github-backup/model"
+	"github.com/MishraShardendu22/github-backup/util"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+)
+
+func LoadEnv() {
+	currEnv := "development"
+
+	if currEnv == "development" {
+		if err := godotenv.Load(); err != nil {
+			util.Logger().Warn("Error loading .env file",
+				zap.Error(err),
+			)
+		}
+	}
+}
+
+func LoadConfig() *model.ConfigModel {
+	return &model.ConfigModel{
+		OrgAccount:          util.GetEnv("ORG_ACCOUNT", ""),
+		MainAccount:         util.GetEnv("MAIN_ACCOUNT", ""),
+		ProjectAccount:      util.GetEnv("PROJECT_ACCOUNT", ""),
+		BackupRepoPath:      util.GetEnv("BACKUP_REPO_PATH", ""),
+		GitHubTokenPrivate:  util.GetEnv("GITHUB_TOKEN_PERSONAL", ""),
+		GitHubTokenPersonal: util.GetEnv("GITHUB_TOKEN_PERSONAL", ""),
+	}
+}
+
+func ImportantURL(config *model.ConfigModel) *model.URL {
+	return &model.URL{
+		GetAllPrivateRepos: "https://api.github.com/user/repos?type=private&per_page=100&page=1",
+		GetAllOrgRepos:     "https://api.github.com/orgs/" + config.OrgAccount + "/repos?type=all&per_page=50&page=",
+		GetAllPublicRepos:  "https://api.github.com/users/" + config.ProjectAccount + "/repos?type=public&per_page=50&page=",
+	}
+}
---
+++ service/backup.service.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+	"github.com/MishraShardendu22/github-backup/config"
+	"github.com/MishraShardendu22/github-backup/controller"
+	"github.com/MishraShardendu22/github-backup/model"
+	"github.com/MishraShardendu22/github-backup/util"
+	"go.uber.org/zap"
+)
+
+func RunBackupFlow() {
+	cfg := config.LoadConfig()
+	urls := config.ImportantURL(cfg)
+
+	allRepos := GetAllRepos(cfg, urls)
+	util.Logger().Info("Repositories loaded",
+		zap.Int("count", len(allRepos)),
+	)
+
+	printRepoList(allRepos)
+	CloneRepos(allRepos, cfg)
+}
+
+func GetAllRepos(config *model.ConfigModel, urls *model.URL) []string {
+	orgReposPersonal := controller.RepoController(urls.GetAllOrgRepos, *config)
+	publicReposPersonal := controller.RepoController(urls.GetAllPublicRepos, *config)
+	privatePersonalAndOrgReops := controller.RepoControllerPrivate(urls.GetAllPrivateRepos, *config)
+
+	var allRepos []string
+	allRepos = append(allRepos, orgReposPersonal...)
+	util.Logger().Info("Org repositories loaded",
+		zap.Int("count", len(orgReposPersonal)),
+	)
+
+	allRepos = append(allRepos, publicReposPersonal...)
+	util.Logger().Info("Public repositories loaded",
+		zap.Int("count", len(publicReposPersonal)),
+	)
+
+	allRepos = append(allRepos, privatePersonalAndOrgReops...)
+	util.Logger().Info("Private repositories loaded",
+		zap.Int("count", len(privatePersonalAndOrgReops)),
+	)
+
+	return allRepos
+}
+
+func printRepoList(repos []string) {
+	for _, repo := range repos {
+		util.Logger().Info("Repository discovered",
+			zap.String("repository", repo),
+		)
+	}
+}
+
+func printBackupSummary(repoNames []string, successCount int, failedRepos []string) {
+	util.Logger().Info("Backup summary",
+		zap.Int("total", len(repoNames)),
+		zap.Int("successful", successCount),
+		zap.Int("failed", len(failedRepos)),
+	)
+
+	if len(failedRepos) > 0 {
+		for _, repo := range failedRepos {
+			util.Logger().Warn("Repository backup failed",
+				zap.String("repository", repo),
+			)
+		}
+	}
+}
